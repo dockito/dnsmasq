c1a08ea3addb71c241fa14c2f0b6f331662bab87
Eduardo Nunes <esnunes@gmail.com>
2015-06-22 09:49:18 -0300

    removed log of dns queries

--- run.sh
+++ run.sh
@@ -6,7 +6,6 @@ if [ "$#" -eq 0 ]; then
   cp -f /etc/dnsmasq.conf.orig /etc/dnsmasq.conf
   echo "nameserver $DOCKITO_DNS" > /etc/resolv.conf
   echo "address=/$DOCKITO_HOST/$DOCKITO_IP" >> /etc/dnsmasq.conf
-  echo "log-queries" >> /etc/dnsmasq.conf
 
   exec dnsmasq -d
 fi
